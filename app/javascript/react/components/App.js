6f3ee953129671f983d8a6df092d14c2407c3647
Brian Lee <blee@Brians-MacBook-Air.local>
2021-12-09 16:15:34 -0500

    Rework results page. Style recipe showpage. Adjust routing

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import HomeIndexPage from "./HomeIndexPage"
 import RecipeShowPage from "./RecipeShowPage"
+import ResultsPage from './ResultsPage'
 
 export const App = (props) => {
   return (
@@ -9,6 +10,7 @@ export const App = (props) => {
       <Switch>
         <Route exact path="/" component={HomeIndexPage} />
         <Route exact path="/recipes" component={HomeIndexPage} />
+        <Route exact path="/results" component={ResultsPage} />
         <Route exact path="/recipes/:id" component={RecipeShowPage} />
       </Switch>
     </BrowserRouter>
